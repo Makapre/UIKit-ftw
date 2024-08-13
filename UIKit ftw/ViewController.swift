a45e407561595ac4bded011684af666e88bf35e4
Marius Preikschat <m.preikschat@googlemail.com>
2024-08-13 12:52:01 +0200

    Initial Commit

---
+++ UIKit ftw/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit ftw
+//
+//  Created by Marius Preikschat on 13.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
